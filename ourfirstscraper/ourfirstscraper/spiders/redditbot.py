a9d11b1a0f2eeeea86f84b2bb79dbee9c361b637
sanad <mohdsanadzakirizvi@gmail.com>
2017-07-21 14:44:16 +0530

    added redditbot

---
+++ ourfirstscraper/ourfirstscraper/spiders/redditbot.py
@@ -0,0 +1,24 @@
+import scrapy
+
+class RedditSpider(scrapy.Spider):
+	name = "redditbot"
+	start_urls = ["https://www.reddit.com/r/gameofthrones/"]
+	custom_settings = {
+		"FEED_URI" : "../tmp/reddit.csv"
+	}
+
+	def parse(self, response):
+		#Extracting the content using css selectors
+		titles = response.css(".title::text").extract()
+		votes = response.css(".score.unvoted::text").extract()
+		times = response.css("time::attr(title)").extract()
+		comments = response.css(".comments::text").extract()
+		#Give the extracted content row wise.
+		for item in zip(titles,votes,times,comments):
+			scraped_info = {
+				"title" : item[0],
+				"vote" : item[1],
+				"created_at" : item[2],
+				"comments" : item[3],
+			}
+			yield scraped_info
